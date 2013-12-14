9fa5e33ad9cb75ddc94a21140c7fdc3aa077c75c
wanderingstan <stan@wanderingstan.com>
2013-12-14 09:28:18 -0800

    better function splitting

---
+++ go_calligraffiti.php
@@ -0,0 +1,17 @@
+<?
+include 'process_all_posts.php'
+
+// Your reddit username
+// (Needed to set user-agent string, per Reddit API: https://github.com/reddit/reddit/wiki/API )
+$reddit_username = "wanderingstan";
+
+// Subreddit to seach in:
+$subreddit = "calligraffiti";
+
+// But only process posts with titles that match this pattern:
+$post_title_re = '/WotD[^"]*"(?P<name>[^"]*)([ "]*)?([0-9]+\/[0-9]+)?/';
+
+// do it!
+process_all_posts();
+
+?>
